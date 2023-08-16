33f60552b57b40523f7b5a159591c394484a06be
carloscastilloflores <125534814+carloscastilloflores@users.noreply.github.com>
2023-08-16 13:49:48 -0600

    added method that accepts data and get data

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,33 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Putting data into the database');
+  try {
+    const contactDB = await openDB('jate', 1);
+    const tx = contactDB.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    await store.put({ id: 1, value: content });
+    console.log('Data saved to the database');
+  } catch (error) {
+    console.error('Error putting data into the database:', error);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const contactDB = await openDB('jate', 1);
+    const tx = contactDB.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('Retrieved data from the database:', result);
+    return result; // No need to access result.value directly
+  } catch (error) {
+    console.error('Error getting data from the database:', error);
+    return []; // Return an empty array or appropriate default value on error
+  }
+};
 
 initdb();
