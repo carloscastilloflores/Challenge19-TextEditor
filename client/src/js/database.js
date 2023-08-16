c8161f7117ae6d9951b4117a833a7ac08807bca6
carloscastilloflores <125534814+carloscastilloflores@users.noreply.github.com>
2023-08-16 17:52:26 -0600

    Solved error about split in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -28,18 +28,24 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  try {
-    const editDB = await openDB('jate', 1);
-    const tx = editDB.transaction('jate', 'readonly');
-    const store = tx.objectStore('jate');
-    const request = store.getAll();
-    const result = await request;
-    console.log('Retrieved data from the database:', result);
-    return result; // No need to access result.value directly
-  } catch (error) {
-    console.error('Error getting data from the database:', error);
-    return []; // Return an empty array or appropriate default value on error
-  }
+  const editdb = await openDB('jate', 1); 
+  const tx = editdb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  return result?.value;
+  // try {
+  //   const editDB = await openDB('jate', 1);
+  //   const tx = editDB.transaction('jate', 'readonly');
+  //   const store = tx.objectStore('jate');
+  //   const request = store.getAll();
+  //   const result = await request;
+  //   console.log('Retrieved data from the database:', result);
+  //   return result; // No need to access result.value directly
+  // } catch (error) {
+  //   console.error('Error getting data from the database:', error);
+  //   return []; // Return an empty array or appropriate default value on error
+  // }
 };
 
 initdb();
