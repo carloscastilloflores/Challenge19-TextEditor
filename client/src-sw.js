30559baa57b4603bba5f1b5b7eb71749fc7a943b
carloscastilloflores <125534814+carloscastilloflores@users.noreply.github.com>
2023-08-16 16:30:00 -0600

    config for managing cache

--- client/src-sw.js
+++ client/src-sw.js
@@ -24,7 +24,22 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(
+  ({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'cache-for-assets',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200], 
+      }),
+      new ExpirationPlugin({
+        maxEntries: 70,
+        maxAgeSeconds: 30 * 24 * 60 * 60, 
+      })
+    ],
+  })
+);
